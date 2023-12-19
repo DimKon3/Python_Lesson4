802621d9d29f1f00aca16ced27363709d52168bc
DimKon3 <142818063+DimKon3@users.noreply.github.com>
2023-12-19 10:17:26 +0300

    Add files via upload

--- Python_Lesson4_2.py
+++ Python_Lesson4_2.py
@@ -1,7 +1,10 @@
+#x = int(input("Введите 5-ое число:"))
+#x4 = x%10
+#x3 = x//10%10
+#x2 = x//100%10
+#x1 = x//1000%10
+#x0 = x//10000
+#print("Результат:",x3**x4*x2/(x0-x1))
+
 giveNumber = input("Введите целое пятизначное число: ")
-print(giveNumber[0])
-print(giveNumber[1])
-print(giveNumber[2])
-print(giveNumber[3])
-print(giveNumber[4])
 print("Result ",(int(giveNumber[3])**int(giveNumber[4]))*int(giveNumber[2])/(int(giveNumber[0])-int(giveNumber[1])))
