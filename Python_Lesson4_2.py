63ae7d3fc726de53ec314ea16c851cc2368cd1a1
DimKon3 <142818063+DimKon3@users.noreply.github.com>
2023-12-27 14:41:58 +0300

    Update Python_Lesson4_2.py

--- Python_Lesson4_2.py
+++ Python_Lesson4_2.py
@@ -1,3 +1,4 @@
+#Вариант 1
 #x = int(input("Введите 5-ое число:"))
 #x4 = x%10
 #x3 = x//10%10
@@ -6,5 +7,6 @@
 #x0 = x//10000
 #print("Результат:",x3**x4*x2/(x0-x1))
 
+#Вариант 2
 giveNumber = input("Введите целое пятизначное число: ")
 print("Result ",(int(giveNumber[3])**int(giveNumber[4]))*int(giveNumber[2])/(int(giveNumber[0])-int(giveNumber[1])))
